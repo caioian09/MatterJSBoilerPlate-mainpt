44a24e911424337a29485cb97c53c60545a682f8
caioian09 <94866906+caioian09@users.noreply.github.com>
2022-02-14 18:28:33 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,18 +18,59 @@ function setup() {
 
 	//Crie os Corpos Aqui.
 
+ 
 
-	Engine.run(engine);
+var block1_options = {
+  restitution:0.5,
+  friction:0.02,
+  frictionAir:0
+	}
+
+var block2_options = {
+  restitution:0.7,
+  friction:0.01,
+  frictionAir:0.1
+	}
+
+var block3_options = {
+  restitution:0.1,
+  friction:1,
+  frictionAir:0.3
+		  }
+
+ block1 = Bodies.circle(220,10,10,block1_options);
+ World.add(world,block1);
+
+ block2 = Bodies.rectangle(110,50,10,10,block2_options);
+ World.add(world,block2);
+
+ block3 = Bodies.rectangle(350,50,10,10,block3_options);
+ World.add(world,block3);
+
+
+	var options = {
+		isStatic: true
+	}
+
+
+	
   
 }
 
 
 function draw() {
+
   rectMode(CENTER);
-  background(0);
-  
+  background("green");
+  rect(block2.position.x, block2.position.y,160,240,);
+  rect(block3.position.x, block3.position.y, width * 2, 1)
+  ellipse(block1.position.x, block1.position.y, width * 2, 1);
   drawSprites();
- Engine
+
+ Engine.update(engine);
+
+
+
 }
 
 
